43a331a23698a536d13093eb6096453dfcf71786
muskankumari03 <muskankumari33300@gmail.com>
2026-06-11 19:31:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+// होम पेज के लिए एक सिंपल वेलकम कंसोल मैसेज (प्रोफेशनल टच)
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Welcome to Muskan Kumari's Modern Portfolio website!");
+});
